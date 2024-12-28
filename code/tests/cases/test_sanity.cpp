617eba0a4db770ea24761f316d13c0eda4439fd0
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-12-28 13:01:12 -0600

    update cases

--- code/tests/cases/test_sanity.cpp
+++ code/tests/cases/test_sanity.cpp
@@ -74,14 +74,15 @@ FOSSIL_TEST_CASE(cpp_parse_args) {
     fossil_sanity_config config;
     fossil_sanity_init_config(&config);
 
-    char *args1[] = {"program", "debug=enable", "logs=disable", "colors=disable", "show=error"};
+    // Change to const char*[] to match string literal type
+    const char *args1[] = {"program", "debug=enable", "logs=disable", "colors=disable", "show=error"};
     fossil_sanity_parse_args(5, args1, &config);
     FOSSIL_TEST_ASSUME(config.debug_enabled == ENABLE, "Debug should be enabled");
     FOSSIL_TEST_ASSUME(config.logs_enabled == DISABLE, "Logs should be disabled");
     FOSSIL_TEST_ASSUME(config.use_colors == DISABLE, "Colors should be disabled");
     FOSSIL_TEST_ASSUME(config.log_level == FOSSIL_SANITY_LOG_ERROR, "Log level should be ERROR");
 
-    char *args2[] = {"program", "no-debug", "logs", "no-colors"};
+    const char *args2[] = {"program", "no-debug", "logs", "no-colors"};
     fossil_sanity_parse_args(4, args2, &config);
     FOSSIL_TEST_ASSUME(config.debug_enabled == DISABLE, "Debug should be disabled");
     FOSSIL_TEST_ASSUME(config.logs_enabled == ENABLE, "Logs should be enabled");
